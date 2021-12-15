377e853c69000e68e2c7bfb337e166e61cbca8b8
leviscoffie <kithinjimiriti4@gmail.com>
2021-12-15 23:35:58 +0300

    Dockerize

--- predict.py
+++ predict.py
@@ -2,10 +2,6 @@ import pickle
 from flask import Flask
 from flask import request
 from flask import jsonify
-<<<<<<< HEAD
-
-=======
->>>>>>> dd2971964e1fbd15afdc0e86eef32d3a7d4ad975
 
 model_file='model_C=1.0.bin'
 
@@ -15,20 +11,6 @@ with open(model_file, 'rb') as f_in:
 app=Flask('churn')
 
 @app.route('/predict',methods=['POST'])
-<<<<<<< HEAD
-def predict():
-    kastama=request.get_json()
-    
-    
-    X=dv.transform([kastama])
-    y_pred=model.predict_proba(X)[0,1]
-    churn=y_pred>=0.5
-    
-    result={
-        'churn_probability':float(y_pred),
-        'churn':bool(churn)
-    }
-=======
 
 def predict():
 
@@ -42,20 +24,10 @@ def predict():
         'churn_probability':float(y_pred),
         'churn':bool(churn)}
     
->>>>>>> dd2971964e1fbd15afdc0e86eef32d3a7d4ad975
     return jsonify(result)
 
 
 
 
 if __name__=="__main__":
-<<<<<<< HEAD
-
-    app.run(debug=True, host="0.0.0.0", port=9696)
-    
-
-    
-
-=======
     app.run(debug=True, host='0.0.0.0', port=9696)
->>>>>>> dd2971964e1fbd15afdc0e86eef32d3a7d4ad975
